14c4242ac65950e63c1d7ee6d6e3f24cd2238130
daniela zimmermann <ceridween@protonmail.com>
2021-03-18 16:21:06 +0100

    CRUD works, without any styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating Ingredients...'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_list = open(url).read
+ingredients = JSON.parse(ingredients_list)
+ingredients_array = ingredients['drinks']
+
+ingredients_array.each do |element|
+  ingredient = element["strIngredient1"]
+  my_ingredient = Ingredient.create!(name: ingredient)
+  puts "Created #{my_ingredient.name}"
+end
+puts 'Finished!'
